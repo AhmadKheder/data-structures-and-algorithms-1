61a2b751fc4c4cb061d8fcb88a1d2d5599c446fa
Osama Hanoun <osamahanoun997@gmail.com>
2020-09-15 00:43:19 +0300

    resubmit class-6

--- challenges/data-structure/__tests__/linkedlist.test.js
+++ challenges/data-structure/__tests__/linkedlist.test.js
@@ -1,50 +1,52 @@
+/* eslint-disable no-undef */
 'use strict';
+// eslint-disable-next-line no-undef
 let LinkedList = require('../linked-list/linkedlist.js');
 
 describe('Linked List Module', () => {
 
-    it('Can successfully instantiate an empty linked list', () => {
-        let list = new LinkedList();
-        expect(list.head).toBeNull();
-    });
+  it('Can successfully instantiate an empty linked list', () => {
+    let list = new LinkedList();
+    expect(list.head).toBeNull();
+  });
 
-    it('insert()', () => {
-        let list = new LinkedList();
-        expect(list.head).toBeNull();
-        let initialValue = 'First One';
-        list.insert(initialValue);
-        expect(list.head.val).toEqual(initialValue);
+  it('insert()', () => {
+    let list = new LinkedList();
+    expect(list.head).toBeNull();
+    let initialValue = 'First One';
+    list.insert(initialValue);
+    expect(list.head.val).toEqual(initialValue);
 
-        let newValue = 'Second Value';
-        list.insert(newValue);
-        expect(list.head.val).toEqual(newValue);
-        expect(list.head.next).not.toBeNull();
-        expect(list.head.next.val).toEqual(initialValue);
+    let newValue = 'Second Value';
+    list.insert(newValue);
+    expect(list.head.val).toEqual(newValue);
+    expect(list.head.next).not.toBeNull();
+    expect(list.head.next.val).toEqual(initialValue);
 
-    });
-    it('includes()', () => {
-        let list = new LinkedList();
-        let initialValue = 'First One';
-        expect(list.includes(initialValue)).toEqual(false);
-        list.insert(initialValue);
-        expect(list.includes(initialValue)).toEqual(true);
+  });
+  it('includes()', () => {
+    let list = new LinkedList();
+    let initialValue = 'First One';
+    expect(list.includes(initialValue)).toEqual(false);
+    list.insert(initialValue);
+    expect(list.includes(initialValue)).toEqual(true);
 
-        let newValue = 'Second Value';
-        list.insert(newValue);
-        expect(list.includes(initialValue)).toEqual(true);
-        expect(list.includes(newValue)).toEqual(true);
-    });
+    let newValue = 'Second Value';
+    list.insert(newValue);
+    expect(list.includes(initialValue)).toEqual(true);
+    expect(list.includes(newValue)).toEqual(true);
+  });
 
-    it('toString()', () => {
-        let list = new LinkedList();
+  it('toString()', () => {
+    let list = new LinkedList();
 
-        list.insert('c');
-        list.insert('b');
-        list.insert('a');
-        expect(list.toString()).toEqual("{ a } -> { b } -> { c } -> NULL");
-    });
+    list.insert('c');
+    list.insert('b');
+    list.insert('a');
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> NULL');
+  });
 
-})
+});
 describe('Challenge 6: expected pass tests', () => {
   it('1. Can successfully add a node to the end of the linked list', () => {
     let newList = new LinkedList();
@@ -53,62 +55,63 @@ describe('Challenge 6: expected pass tests', () => {
     newList.insert('B');
     newList.insert('C');
     newList.append('D');
-    
+
     expect(newList.toString()).toEqual('{ C } -> { B } -> { A } -> { D } -> NULL');
   });
 
   it('2. Can successfully add multiple nodes to the end of a linked list', () => {
-      let newList = new LinkedList();
-  
-      newList.insert('A');
-      newList.insert('B');
-      newList.insert('C');
-      newList.append('D');
-      newList.append('E');
-      expect(newList.toString()).toEqual('{ C } -> { B } -> { A } -> { D } -> { E } -> NULL');
-    });
+    let newList = new LinkedList();
+
+    newList.insert('A');
+    newList.insert('B');
+    newList.insert('C');
+    newList.append('D');
+    newList.append('E');
+    expect(newList.toString()).toEqual('{ C } -> { B } -> { A } -> { D } -> { E } -> NULL');
+  });
 
   it('3. Can successfully insert a node before a node located i the middle of a linked list', () => {
-      let newList = new LinkedList();
-  
-      newList.insert('A');
-      newList.insert('B');
-      newList.insert('C');
-      newList.insert('D');
-      newList.insertBefore('C', 'BtoC');
-      expect(newList.toString()).toEqual('{ D } -> { BtoC } -> { C } -> { B } -> { A } -> NULL');
-    });
+    let newList = new LinkedList();
 
-    it('4. Can successfully insert a node before the first node of a linked list', () => {
-      let newList = new LinkedList();
-  
-      newList.insert('A');
-      newList.insert('B');
-      newList.insert('C');
-      newList.insert('D');
-  
-      expect(newList.toString()).toEqual('{ D } -> { C } -> { B } -> { A } -> NULL');
-    });
+    newList.insert('A');
+    newList.insert('B');
+    newList.insert('C');
+    newList.insert('D');
+    newList.insertBefore('C', 'BtoC');
+    expect(newList.toString()).toEqual('{ D } -> { BtoC } -> { C } -> { B } -> { A } -> NULL');
+  });
 
-    it('5. Can successfully insert after a node in the middle of the linked list', () => {
-      let newList = new LinkedList();
-  
-      newList.insert('A');
-      newList.insert('B');
-      newList.insert('C');
-      newList.insert('D');
-      newList.insertAfter('B', 'BtoA');
-      
-      expect(newList.toString()).toEqual('{ D } -> { C } -> { B } -> { BtoA } -> { A } -> NULL');
-    });
+  it('4. Can successfully insert a node before the first node of a linked list', () => {
+    let newList = new LinkedList();
 
-    it('6. Can successfully insert a node after the last node of the linked list', () => {
-      let newList = new LinkedList();
-  
-      newList.insert('A');
-      newList.insert('B');
-      newList.insert('C');
-      newList.append('D');
-      expect(newList.toString()).toEqual('{ C } -> { B } -> { A } -> { D } -> NULL');
-    });
+    newList.insert('A');
+    newList.insert('B');
+    newList.insert('C');
+    newList.insert('D');
+
+    expect(newList.toString()).toEqual('{ D } -> { C } -> { B } -> { A } -> NULL');
+  });
+
+  it('5. Can successfully insert after a node in the middle of the linked list', () => {
+    let newList = new LinkedList();
+
+    newList.insert('A');
+    newList.insert('B');
+    newList.insert('C');
+    newList.insert('D');
+    newList.insertAfter('B', 'BtoA');
+
+    // eslint-disable-next-line no-undef
+    expect(newList.toString()).toEqual('{ D } -> { C } -> { B } -> { BtoA } -> { A } -> NULL');
+  });
+
+  it('6. Can successfully insert a node after the last node of the linked list', () => {
+    let newList = new LinkedList();
+
+    newList.insert('A');
+    newList.insert('B');
+    newList.insert('C');
+    newList.append('D');
+    expect(newList.toString()).toEqual('{ C } -> { B } -> { A } -> { D } -> NULL');
+  });
 });
---
+++ challenges/data-structure/linked-list/README.md
@@ -0,0 +1,21 @@
+# Challenge Summary
+Code Challenge: Class 06
+To write three methods for the linked-list which are .append(value),.insertBefore(value, newVal), .insertAfter(value, newVal).
+
+## Challenge Description
+Code Challenge: Class 06
+Write the following methods for the Linked List class:
+* .append(value) which adds a new node with the given value to the end of the list
+* .insertBefore(value, newVal) which add a new node with the given newValue immediately before the first value node
+* .insertAfter(value, newVal) which add a new node with the given newValue immediately after the first value node
+
+## Approach & Efficiency
+Code Challenge: Class 06
+O(n)
+
+## Solution
+
+Code Challenge: Class 06
+![](./assets/class-06-1.PNG)
+![](./assets/class-06-2.PNG)
+![](./assets/class-06-3.PNG)
